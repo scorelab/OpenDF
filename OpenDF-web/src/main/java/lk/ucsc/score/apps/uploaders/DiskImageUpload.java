2295c9b51b4d0066f22e5e35d54aa08f1783bcee
agentmilindu <agentmilindu@gmail.com>
2014-09-09 12:42:07 +0530

    FileUploading added

---
+++ OpenDF-web/src/main/java/lk/ucsc/score/apps/uploaders/DiskImageUpload.java
@@ -0,0 +1,167 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.ucsc.score.apps.uploaders;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Acer
+ */
+@WebServlet(name = "DiskImageUpload", urlPatterns = {"/DiskImageUpload"})
+@MultipartConfig
+public class DiskImageUpload extends HttpServlet {
+
+    final String path = "uploads";
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DiskImageUpload</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+
+            final Part filePart = request.getPart("file");
+            final String fileName = getFileName(filePart);
+            OutputStream outStream = null;
+            InputStream filecontent = null;
+            final PrintWriter writer = response.getWriter();
+
+            try {
+                outStream = new FileOutputStream(new File(path + File.separator
+                        + fileName));
+                filecontent = filePart.getInputStream();
+
+                int read = 0;
+                final byte[] bytes = new byte[1024];
+
+                while ((read = filecontent.read(bytes)) != -1) {
+                    outStream.write(bytes, 0, read);
+                }
+                out.println("<h1>Servlet DiskImageUpload at " + request.getContextPath() + "</h1>");
+                out.println("<p>New file " + fileName + " created at " + path);
+
+                out.println("</body>");
+                out.println("</html>");
+
+            } catch (FileNotFoundException fne) {
+                out.println("<h1>Servlet DiskImageUpload at " + request.getContextPath() + "</h1>");
+                out.println("You either did not specify a file to upload or are "
+                        + "trying to upload a file to a protected or nonexistent "
+                        + "location.");
+                out.println("<br/> ERROR: " + fne.getMessage());
+                
+               
+
+                out.println("</body>");
+                out.println("</html>");
+
+
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+                if (filecontent != null) {
+                    filecontent.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        File uploads = new File(path);
+        if (!uploads.exists()) {
+            uploads.mkdir();
+        }
+    }
+
+    private String getFileName(Part filePart) {
+        String header = filePart.getHeader("content-disposition");
+        for (String content : header.split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+}

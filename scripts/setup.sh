68355bc867fae44407ae015ece94d5ff17929f63
philipjar <philipjar@gmail.com>
2016-01-03 17:27:58 +0100

    Corrected db access and added automake install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,11 @@ echo "Setup in progress!"
 
 cd /home/OpenDF
 
-# Install maven and mysql
+# Install maven and mysql and automake
 # The following two lines prevent mysql from asking for a password (it will be set to rooot)
 echo mysql-server mysql-server/root_password password rooot | debconf-set-selections
 echo mysql-server mysql-server/root_password_again password rooot | debconf-set-selections
-apt-get -y install maven mysql-server mysql-client
+apt-get -y install maven mysql-server mysql-client automake
 
 # Build OpenDF
 mvn install:install-file -Dfile=sleuthkit/bindings/java/dist/Tsk_DataModel.jar \
@@ -18,10 +18,10 @@ mvn clean install
 
 # Setting up mysql
 service mysql start
-mysql --execute="CREATE DATABASE IF NOT EXISTS OpenDF;"
-mysql --execute="CREATE USER IF NOT EXISTS 'OpenDFU'@'localhost' IDENTIFIED BY '123';"
-mysql --execute="GRANT ALL PRIVILEGES ON OpenDF.* TO 'OpenDFU'@'localhost';"
-mysql --execute="SOURCE db/OpenDF.sql"
+mysql -u root --password="rooot" --execute="CREATE DATABASE IF NOT EXISTS OpenDF;"
+mysql -u root --password="rooot" --execute="CREATE USER IF NOT EXISTS 'OpenDFU'@'localhost' IDENTIFIED BY '123';"
+mysql -u root --password="rooot" --execute="GRANT ALL PRIVILEGES ON OpenDF.* TO 'OpenDFU'@'localhost';"
+mysql -u root --password="rooot" --execute="SOURCE db/OpenDF.sql"
 
 # Build dependencies
 # Not using the build_sleuthkit script here as it is interactive

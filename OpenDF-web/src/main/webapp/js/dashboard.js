fb63b4d87cd42f18751d368f535f9f15c8309312
Lucas Jones <lucasjonesdev@hotmail.co.uk>
2016-01-21 21:13:57 +0000

    Add buttons to select between list and thumbnail file view
    Persists the user's choice in local storage.

--- OpenDF-web/src/main/webapp/js/dashboard.js
+++ OpenDF-web/src/main/webapp/js/dashboard.js
@@ -204,7 +204,7 @@ OpenDFApp.controller('notesController', ['$scope', '$routeParams', 'notesFactory
             $scope.notes  = notes;
         });
         $scope.isFileType = function(name, types){
-            return types.indexOf(name.match(/\.[0-9a-z]{1,5}$/i)[0]) >= 0;
+            return types.indexOf((name.match(/\.[0-9a-z]{1,5}$/i) || [undefined])[0]) >= 0;
         }
         $scope.hasFileType = function(name){
             return name.match(/\.[0-9a-z]{1,5}$/i)!=null;
@@ -307,7 +307,10 @@ services.factory('InvestigatorsFactory', function ($resource) {
         })
 });
 OpenDFApp.controller('filesController', ['$scope', '$location', '$routeParams' , 'DiskImagesFactory', function ($scope, $location, $routeParams, DiskImagesFactory) {
-       $scope.displayType = 'thumbs';
+        $scope.displayFormat = localStorage.fileDisplayFormat || 'link';
+        $scope.$watch('displayFormat', function() {
+            localStorage.fileDisplayFormat = $scope.displayFormat;
+        })
        
         $scope.getFilesByHierarchy = function(){
             DiskImagesFactory.getFile({ id: $routeParams.idProject,  idFile: $routeParams.idFile  }, function(data) {
@@ -322,7 +325,7 @@ OpenDFApp.controller('filesController', ['$scope', '$location', '$routeParams' ,
             });
         }
         $scope.isFileType = function(name, types){
-            return types.indexOf(name.match(/\.[0-9a-z]{1,5}$/i)[0]) >= 0;
+            return types.indexOf((name.match(/\.[0-9a-z]{1,5}$/i) || [undefined])[0]) >= 0;
         }
         $scope.hasFileType = function(name){
             return name.match(/\.[0-9a-z]{1,5}$/i)!=null;
@@ -336,11 +339,10 @@ OpenDFApp.directive('dfThumbBackgroundImage', function () {
         },
         link: function (scope, elem, attrs) {
             function isFileType(name, types) {
-                return types.indexOf(name.match(/\.[0-9a-z]{1,5}$/i)[0]) >= 0;
+                return types.indexOf((name.match(/\.[0-9a-z]{1,5}$/i) || [undefined])[0]) >= 0;
             }
             scope.$watch('dfThumbBackgroundImage', function (file) {
                 var isImage = isFileType(file.name, ['.png', '.jpg', '.jpeg', '.gif', '.tif']);
-                console.log('file', file, isImage);
                 if (isImage) {
                     elem.css({
                         'background-image': 'url(ServeFile?idFile=' + file.idFile + ')',

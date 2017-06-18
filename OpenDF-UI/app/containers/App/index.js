51469b3cf472d69bb656e8c53da4fd09b70b051f
pmartz92 <pmartz92@gmail.com>
2017-06-18 17:55:15 +0530

    Material UI Added

--- OpenDF-UI/app/containers/App/index.js
+++ OpenDF-UI/app/containers/App/index.js
@@ -12,6 +12,10 @@
  */
 
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -21,9 +25,11 @@ export default class App extends React.PureComponent { // eslint-disable-line re
 
   render() {
     return (
-      <div>
+      <MuiThemeProvider>
+        <div>
         {React.Children.toArray(this.props.children)}
-      </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }

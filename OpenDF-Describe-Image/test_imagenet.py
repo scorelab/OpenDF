5af716b59b1077c4abd8f4f4be8b83189d0490c3
Nishan Gunawardena <shehan.weer123@gmail.com>
2017-06-20 23:37:52 +0530

    [#67] Remove unnecessary comments

--- OpenDF-Describe-Image/test_imagenet.py
+++ OpenDF-Describe-Image/test_imagenet.py
@@ -1,7 +1,6 @@
 # USAGE
 # python test_imagenet.py --image images/dog_beagle.png
 
-# import the necessary packages
 from keras.preprocessing import image as image_utils
 from imagenet_utils import decode_predictions
 from imagenet_utils import preprocess_input
@@ -10,41 +9,27 @@ import numpy as np
 import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
 args = vars(ap.parse_args())
 
-# load the original image via OpenCV so we can draw on it and display
-# it to our screen later
 orig = cv2.imread(args["image"])
 
-# load the input image using the Keras helper utility while ensuring
-# that the image is resized to 224x224 pxiels, the required input
-# dimensions for the network -- then convert the PIL image to a
-# NumPy array
 print("[INFO] loading and preprocessing image...")
 image = image_utils.load_img(args["image"], target_size=(224, 224))
 image = image_utils.img_to_array(image)
 
-# our image is now represented by a NumPy array of shape (3, 224, 224),
-# but we need to expand the dimensions to be (1, 3, 224, 224) so we can
-# pass it through the network -- we'll also preprocess the image by
-# subtracting the mean RGB pixel intensity from the ImageNet dataset
 image = np.expand_dims(image, axis=0)
 image = preprocess_input(image)
 
-# load the VGG16 network
 print("[INFO] loading network...")
 model = VGG16(weights="imagenet")
 
-# classify the image
 print("[INFO] classifying image...")
 preds = model.predict(image)
 (inID, label) = decode_predictions(preds)[0]
 
-# display the predictions to our screen
 print("ImageNet ID: {}, Label: {}".format(inID, label))
 cv2.putText(orig, "Label: {}".format(label), (10, 30),
 	cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)

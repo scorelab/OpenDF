156e1b8aaeb2d37bb5c619c8730b1cef0684903c
ShehanWeerakkody <Nishan123>
2017-07-22 08:53:16 +0530

    [#68] Remove POST method

--- OpenDF-Describe-Image/src/Server/server.py
+++ OpenDF-Describe-Image/src/Server/server.py
@@ -28,10 +28,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-#de get_actual_path(uuid):
-#	images = {'cfgvhbjnmk':'uploded/image1.jpg'}
-#	retu iimags[uuid]
-
 def predictImage(modelType, imagePath):
 	print("[INFO] loading and preprocessing image...")
 	image = image_utils.load_img(imagePath, target_size=(224, 224))
@@ -64,34 +60,9 @@ def allowed_file(filename):
 @app.route('/api/analyze/<path:path>', methods=['GET'])
 def uploadImage(path):
 	IPath = "/"+path
-	#print(IPath)
 	if(os.path.isfile(IPath)):
-		#return jsonify({"predicted" : IPath})
-		#process = subprocess.call("python ../main/main.py --model VGG16 --image " + IPath, shell=True)
 		value = predictImage("VGG16",IPath)
-		#print(process)
 	return jsonify({"predicted" : value})
 	
-	
-
-
-@app.route('/api/analyze/<path:path>', methods=['POST'])
-def returnPrediction(path):
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #return redirect(url_for('uploaded_file',filename=filename))
-        return jsonify({"predicted" : "House"})
-
 if __name__ == '__main__':
     app.run(debug=True)

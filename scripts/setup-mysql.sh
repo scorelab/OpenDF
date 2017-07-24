9cacc3a256feebf339db054e39d2679eb5563ee1
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2017-07-25 00:36:11 +0530

    [#74] Update MySQL setup scripts

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -3,11 +3,8 @@
 # Setting up mysql
 echo "Setting up mysql!"
 
-mysqld
 
 mysql -u $MYSQL_USER --password="$MYSQL_PW" --execute="CREATE DATABASE IF NOT EXISTS OpenDF;"
 mysql -u $MYSQL_USER --password="$MYSQL_PW" --execute="CREATE USER '${OPENDF_DB_USER}'@'%' IDENTIFIED BY '${OPENDF_DB_PW}';"
 mysql -u $MYSQL_USER --password="$MYSQL_PW" --execute="GRANT  INSERT, SELECT, UPDATE, DELETE ON OpenDF.* TO '${OPENDF_DB_USER}'@'%';"
 mysql -u $MYSQL_USER --password="$MYSQL_PW" OpenDF < /OpenDF/db/OpenDF.sql
-
-service mysql restart

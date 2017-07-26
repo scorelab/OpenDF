0e7142e924338277400f937d6bade2c9c297fbd9
ShehanWeerakkody <Nishan123>
2017-07-27 00:24:33 +0530

    [#72] Implement two HTTP Error responses(valid uuid and file not found)

---
+++ OpenDF-Describe-Image/src/Server/exceptions/InvalidUUID.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+
+
+class InvalidUUID(Exception):
+
+    def __init__(self, payload=None):
+        Exception.__init__(self)
+        self.message = "Bad Request - UUID Not Valid"
+        self.status_code = 400
+        self.payload = payload
+
+    def to_dict(self):
+        error = dict(self.payload or ())
+        error['message'] = self.message
+        return error
